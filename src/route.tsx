8baf7b105f74ca8ff7563d402d5c4507833c893b
Gagan459 <147545501+Gagan459@users.noreply.github.com>
2026-09-19 10:22:06 +0530

    Revert "Add Drive Students page for best fit recommendations with filter "

--- src/route.tsx
+++ src/route.tsx
@@ -7,7 +7,6 @@ import { RouterError } from "./components/routerError";
 import { CampusDrive } from "./components/campusDrive";
 import { DriveDetails } from "./components/driveDetails";
 import { JDRecommendation } from "./components/jdRecommendation";
-import { DriveStudents } from "./components/driveStudents";
 import Login from "./components/login";
 import  { AddDrive } from "./components/addDrive";
 import { AnnouncementsRoute } from "./components/announcementsRoute";
@@ -57,14 +56,6 @@ export const router = createBrowserRouter([
       {
         path: "jdrecommendation",
         element: <JDRecommendation />,
-      },
-      {
-        path: "jdrecommendation/:driveId/students",
-        element: <DriveStudents />,
-      },
-      {
-        path: "jdrecommendation/students",
-        element: <DriveStudents />,
       },
        {
         path: "add_drives",
